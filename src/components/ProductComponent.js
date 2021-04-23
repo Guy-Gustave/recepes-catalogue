ffa5de0fceb4bf3910ca618a8934124db898cc16
Guy <guymoustapha@gmail.com>
2021-04-23 14:37:39 +0200

    display product component into product listing

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 
 const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const { id, title } = products;
   return (
-    <div>
-      <h1>ProductComponent</h1>
+    <div four column wide>
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"></div>
+          <div className="content">
+            <div className="header">{ title }</div>
+          </div>
+        </div>
+      </div>      
     </div>
   );
 };
