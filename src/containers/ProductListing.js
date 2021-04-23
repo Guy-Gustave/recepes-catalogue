90cae02af244974e2ba4b2ccb975d4f98d7df434
Guy <guymoustapha@gmail.com>
2021-04-23 14:38:13 +0200

    fetch data from API using axios

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import ProductComponent from '../components/ProductComponent';
 
 const ProductListing = () => {
+  const products = useSelector((state) => state);
+
+  const fetchProducts = async () => {
+    const response = await axios
+    .get ("https://vpic.nhtsa.dot.gov/api/vehicles/GetWMIsForManufacturer/hon?format=json")
+    .catch((err)=> {
+      console.log("Err", err);
+    });
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products);
+  
   return (
     <div>
-      <h1>ProductListing</h1>
+      <ProductComponent />
     </div>
   );
 };
