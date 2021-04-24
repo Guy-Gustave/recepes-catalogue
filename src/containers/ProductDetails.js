8a57b203d7d1a6a19f5affb857336022bf6b49ec
Guy <guymoustapha@gmail.com>
2021-04-24 14:27:47 +0200

    display id of product retrieved

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
 
 const ProductDetails = () => {
+  const  { productId } = useParams();
+  console.log(productId);
   return (
     <div>
       <h1>ProductDetails</h1>
