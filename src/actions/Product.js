aa06f9d5e6b494388c3a7be1368c6387504a7935
Guy <guymoustapha@gmail.com>
2021-04-26 19:28:05 +0200

    fetching data from api with axios

--- src/actions/Product.js
+++ src/actions/Product.js
@@ -13,4 +13,3 @@ export const selectedProduct = (product) => {
     payload: product,
   };
 };
-
--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,11 +1,25 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
 import axios from 'axios';
+import { selectedProduct } from '../actions/Product';
 
 
 const ProductDetails = () => {
+  const product = useSelector(state => state.product)
   const  { productId } = useParams();
+  const dispatch = useDispatch();
   console.log(productId);
+
+  const fetchProductsDetail = async () => {
+    const response = await axios.get(`https://fakestoreapi.com/products/${productId}`).catch((err) => {
+      console.log('err', err);
+    });
+  dispatch(selectedProduct(response.data));
+  }
+  useEffect(() => {
+    if (productId && productId !== "") fetchProductsDetail()
+  }, [productId])
   return (
     <div>
       <h1>ProductDetails</h1>
