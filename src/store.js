522a9cbef6243828027d5affc0de7537cd3010dc
Guy <guymoustapha@gmail.com>
2021-04-27 12:34:53 +0200

    fix eslint  errors

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import reducers from './reducers/index';
 const store = createStore(
   reducers,
   {},
+  // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
