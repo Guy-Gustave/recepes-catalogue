7e4917bddddf9d58156b0b17a012468d7bdef546
Guy <guymoustapha@gmail.com>
2021-04-30 16:53:37 +0200

    fix eslint offenses

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import Header from '../components/Header';
 
 it('renders correctly', () => {
